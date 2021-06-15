95b00fc024dbfd118db450219a7a2190d08df3bd
jeff <sc18j3j@leeds.ac.uk>
2021-06-15 16:27:00 +0100

    feat: moveit collision objects

--- src/utilities/gpd_mask_rcnn.py
+++ src/utilities/gpd_mask_rcnn.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from ycb_mask_rcnn import *
 
 if __name__ == '__main__':
@@ -23,7 +24,7 @@ if __name__ == '__main__':
                 target = i
 
     if target is not None:
-        pclmsg = clouds[i]
+        pclmsg = clouds[target]
         pcl = np.fromstring(pclmsg.data, dtype=np.float32)
         pcl = pcl.reshape(pclmsg.height, pclmsg.width, -1)
 
@@ -41,18 +42,56 @@ if __name__ == '__main__':
         from std_msgs.msg import Header, Int64
         from geometry_msgs.msg import Point
 
-        pub = rospy.Publisher('cloud_indexed', CloudIndexed, queue_size=1)
+        gpd_pub = rospy.Publisher('cloud_indexed', CloudIndexed, queue_size=1)
+        pcl_pub = rospy.Publisher('gpd_segmented_cloud', PointCloud2, queue_size=1, latch=1)
+
+        pclmsg_out = ros_numpy.msgify(PointCloud2, cloud)
+        pclmsg_out.header = pclmsg.header
 
         msg = CloudIndexed()
-        header = Header()
-        header.frame_id = "/base_link"
-        header.stamp = rospy.Time.now()
-        msg.cloud_sources.cloud = ros_numpy.msgify(PointCloud2, cloud)
+        msg.cloud_sources.cloud = pclmsg_out
         msg.cloud_sources.view_points.append(Point(0,0,0))
         for i in xrange(cloud.shape[0]):
             msg.cloud_sources.camera_source.append(Int64(0))
             msg.indices.append(Int64(i))
 
-        pub.publish(msg)
-        rospy.sleep(2)
-        print 'Published cloud with', len(msg.indices), 'indices'
+        gpd_pub.publish(msg)
+        pcl_pub.publish(pclmsg_out)
+
+
+        # create moveit collision object
+        from moveit_msgs.msg import CollisionObject
+        from shape_msgs.msg import SolidPrimitive
+        from geometry_msgs.msg import Pose
+
+        co_pub = rospy.Publisher('/collision_object', CollisionObject, queue_size=100)
+        rospy.sleep(1)
+
+        co = CollisionObject()
+        co.header = pclmsg.header
+        co.id = 'target_object'
+        co.operation = CollisionObject.ADD
+
+        primitive = SolidPrimitive()
+        primitive.type = primitive.BOX
+        primitive.dimensions = [0.02, 0.02, 0.02]
+
+        no_samples = 200
+        indices = np.random.randint(0, cloud.shape[0], no_samples)
+        for i in indices:
+            x,y,z = cloud[i]
+            primitive_pose = Pose()
+            primitive_pose.position.x = x
+            primitive_pose.position.y = y
+            primitive_pose.position.z = z
+            primitive_pose.orientation.x = 0
+            primitive_pose.orientation.y = 0
+            primitive_pose.orientation.z = 0
+            primitive_pose.orientation.w = 1
+            co.primitives.append(primitive)
+            co.primitive_poses.append(primitive_pose)
+
+        co_pub.publish(co)
+
+        print 'Published cloud with', len(msg.indices), 'indices, CTRL+C to exit'
+        rospy.spin()
